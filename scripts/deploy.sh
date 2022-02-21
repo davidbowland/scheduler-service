7761558ca4cc5ae239ef6ca6fa11fd1d6654a7c4
David Bowland <dbowland1@gmail.com>
2022-02-21 14:06:20 -0600

    Add error subscription

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Stop immediately on error
+set -e
+
+if [[ -z "$1" ]]; then
+  $(./scripts/assumeDeveloperRole.sh)
+fi
+
+# Build from template
+
+SAM_TEMPLATE=template.yaml
+sam build --template ${SAM_TEMPLATE} --use-container
+
+# Deploy build lambda
+
+TESTING_ARTIFACTS_BUCKET=dbowland-lambda-east-2-test
+TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/dbowland-cloudformation-test"
+TESTING_STACK_NAME=scheduler-service-test
+sam deploy --stack-name ${TESTING_STACK_NAME} \
+           --capabilities CAPABILITY_IAM \
+           --region us-east-2 \
+           --s3-bucket ${TESTING_ARTIFACTS_BUCKET} \
+           --no-fail-on-empty-changeset \
+           --role-arn ${TESTING_CLOUDFORMATION_EXECUTION_ROLE} \
+           --parameter-overrides "Environment=test"
